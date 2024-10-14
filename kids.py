c844cc17a5c7eae3168c3e3a64e6e500b8e24e23
idk-aarza <aarzak2012@gmail.com>
2024-10-14 18:05:59 +0530

    dolphin

---
+++ kids.py
@@ -0,0 +1,9 @@
+message="congratulations"
+message2='on'
+message3='getting'
+message4='the'
+message5='gold'
+message6="medal"
+message7='Ram'
+print(message, message2, message3, message4, message5, message6, message7, '!')
+print(message.upper(), message2.upper(), message3.upper(), message4.upper(), message5.upper(), message6.upper(), message7.upper(), end= '!')
